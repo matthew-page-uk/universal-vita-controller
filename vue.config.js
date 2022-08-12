b8fe930ab31f78d8e1bf1a972537a2287055d49b
Matthew Page <matthew@aslivemedia.com>
2022-08-12 14:54:17 +0100

    use package.json version on titlebar

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const fs = require('fs');
+const packageJson = fs.readFileSync('./package.json');
+const version = JSON.parse(packageJson).version || 0;
+
+process.env.VUE_APP_VERSION = version;
+
 module.exports = defineConfig({
-  transpileDependencies: true,
-  pluginOptions: {
-    electronBuilder: {
-      preload: 'src/preload.js',
-    }
-  }
+    transpileDependencies: true,
+    pluginOptions: {
+        electronBuilder: {
+            preload: 'src/preload.js',
+        }
+    },
 });
